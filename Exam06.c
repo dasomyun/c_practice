f5125e9c6b475b81b048a34639bf5c32bc29f65b
dasomyun <dasom3020@daum.net>
2017-01-15 03:07:51 +0900

    printf, scanf 정리, 반복문

---
+++ Exam06.c
@@ -0,0 +1,32 @@
+//06. prinf함수와 scanf함수 정리하기
+/*
+#include <stdio.h>
+
+int main(void) {
+	printf("앞집 강아지가 \"멍~! 멍~! \"\n ");
+	int myage = 12;
+	printf("제 나이는 10진수로 %d살 16진수로 %X살 입니다\n", myage, myage);
+	printf("%-8s %14s %5s \n", "이    름", "전공학과", "학년");
+	printf("%-8s %14s %5d \n", "김동수", "전자공학", 3);
+	printf("%-8s %14s %5d \n", "이을수", "컴퓨터공학", 2);
+
+	float num1;
+	double num2;
+	long double num3;
+
+	printf("실수 입력1(e표기법으로): ");
+	scanf("%f", &num1);
+	printf("입력된 실수 %f \n", num1);
+
+	printf("실수 입력2(e표기법으로): ");
+	scanf("%lf", &num2);
+	printf("입력된 실수 %f \n", num2);
+	
+	printf("실수 입력3(e표기법으로): ");
+	scanf("%Lf", &num3);
+	printf("입력된 실수 %f \n", num3);
+	
+	return 0;
+
+}
+*/
